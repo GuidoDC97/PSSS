9156cd83db3501e46c7ef4c9b6eedb5b6d06d0d0
GuidoDC97 <guidodichiara19@gmail.com>
2020-08-27 18:26:25 +0200

    Docenti

--- registroElettronico/backend/src/main/java/com/psss/registroElettronico/backend/controller/DocenteController.java
+++ registroElettronico/backend/src/main/java/com/psss/registroElettronico/backend/controller/DocenteController.java
@@ -1,23 +1,45 @@
 package com.psss.registroElettronico.backend.controller;
 
 
-import com.psss.registroElettronico.backend.service.StudenteService;
+import com.psss.registroElettronico.backend.model.Docente;
+import com.psss.registroElettronico.backend.service.DocenteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("docente")
+@RequestMapping("docenti")
 public class DocenteController {
 
     @Autowired
-    StudenteService docenteService;
+    DocenteService docenteService;
 
     @GetMapping
-    public void getDocenti() {
+    public List<Docente> getDocenti() {
+        return docenteService.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Optional<Docente> getDocente(@PathVariable Long id){
+        return docenteService.findById(id);
+    }
+
+    @GetMapping
+    public List<Docente> getDocenteByNomeCognome(@RequestBody String nome, @RequestBody String cognome){
+        return docenteService.findByNomeCognome(nome, cognome);
 
     }
 
+    @PostMapping
+    public Docente create(@RequestBody final Docente docente){
+        return docenteService.save(docente);
+    }
 
+    @RequestMapping(value="{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        docenteService.deleteById(id);
+    }
 }
--- registroElettronico/backend/src/main/java/com/psss/registroElettronico/backend/service/DocenteService.java
+++ registroElettronico/backend/src/main/java/com/psss/registroElettronico/backend/service/DocenteService.java
@@ -3,15 +3,19 @@ package com.psss.registroElettronico.backend.service;
 import com.psss.registroElettronico.backend.model.Docente;
 import com.psss.registroElettronico.backend.repository.DocenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class DocenteService {
 
     @Autowired
     private DocenteRepository docenteRepository;
 
+    @GetMapping
     public List<Docente> findAll() {
         return docenteRepository.findAll();
     }
@@ -25,7 +29,7 @@ public class DocenteService {
     }
 
     public Docente save(Docente d) {
-        return docenteRepository.save(d);
+        return docenteRepository.saveAndFlush(d);
     }
 
     public void deleteById(Long id) {
