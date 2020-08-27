f9b24192c2d6a2ebe1942f2256e5bf7d2f851219
Fabio d'Andrea <fabio.dandrea.1997@gmail.com>
2020-08-27 11:16:03 +0200

    Inizializzazione progetto

---
+++ registroElettronico/src/main/java/com/psss/registroElettronico/RegistroElettronicoApplication.java
@@ -0,0 +1,13 @@
+package com.psss.registroElettronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroElettronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroElettronicoApplication.class, args);
+	}
+
+}
---
+++ registroElettronico/src/test/java/com/psss/registroElettronico/RegistroElettronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psss.registroElettronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroElettronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
