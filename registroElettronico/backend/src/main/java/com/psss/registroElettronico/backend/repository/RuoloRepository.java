c1c94bf26fa349de1584e6d57931c0992706375b
Fabio d'Andrea <fabio.dandrea.1997@gmail.com>
2020-08-28 16:33:02 +0200

    Schifo

--- registroElettronico/backend/src/main/java/com/psss/registroElettronico/backend/repository/RuoloRepository.java
+++
@@ -1,7 +0,0 @@
-package com.psss.registroElettronico.backend.repository;
-
-import com.psss.registroElettronico.backend.model.Ruolo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RuoloRepository extends JpaRepository<Ruolo, Long> {
-}
